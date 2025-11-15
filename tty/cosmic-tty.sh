f136e5ab80aceaa56c0643e744fde365a3c66b0d
Gerard <gerardbajona@gmail.com>
2025-11-15 20:08:21 +0100

    Add tty support

---
+++ tty/cosmic-tty.sh
@@ -0,0 +1,19 @@
+if [[ "$TERM" = "linux" ]]; then
+	echo -en "\e]P00F1419"
+	echo -en "\e]P1FB6F92"
+	echo -en "\e]P25AB220"
+	echo -en "\e]P3C39720"
+	echo -en "\e]P427A7EC"
+	echo -en "\e]P5E66AFA"
+	echo -en "\e]P623B0A3"
+	echo -en "\e]P76C7885"
+	echo -en "\e]P8141A20"
+	echo -en "\e]P9EA92A4"
+	echo -en "\e]PA78BC5C"
+	echo -en "\e]PBCBA65D"
+	echo -en "\e]PC6FB1E7"
+	echo -en "\e]PDD991E6"
+	echo -en "\e]PE62BAB0"
+	echo -en "\e]PFA3ACB7"
+	clear #for background artifacting
+fi
